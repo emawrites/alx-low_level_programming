4b3f66265f1f3c955cf8985161c2284404ceefd1
emawrites <talkema2@gmail.com>
2022-04-08 06:08:20 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main -Entr point
+*My hello, printf task
+*Returns: return 1 after program execution 
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
