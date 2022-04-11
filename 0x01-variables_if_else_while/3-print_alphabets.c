180edef8fe680a4e9370c4df26e8ef35e59a1cb5
emawrites <talkema2@gmail.com>
2022-04-11 07:10:17 -0700

    new push

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,12 +15,12 @@ int main(void)
 
 	for (lower_case = 'a'; lower_case <= 'z' ; lower_case++)
 	{
-		putchar(upper_case);
+		putchar(lower_case);
 	}
 
 	for (upper_case = 'A'; upper_case <= 'Z' ; upper_case++)
 	{
-		putchar(lower_case);
+		putchar(upper_case);
 	}
 
 	putchar('\n');
