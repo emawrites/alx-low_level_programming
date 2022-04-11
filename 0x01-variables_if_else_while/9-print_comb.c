19ed28e49b9daa1c88469ec4953ed943740bfcea
emawrites <talkema2@gmail.com>
2022-04-11 08:07:20 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n:
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+
+	if (n == 57)
+	{
+		break;
+	}
+
+	putchar(',');
+	putchar(' ');
+
+	return (0);
+}
