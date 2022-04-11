05bd1f3f7b8a6375af268bba53fe551dba717d93
emawrites <talkema2@gmail.com>
2022-04-11 07:49:09 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,19 +5,19 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		printf("%i", i);
+		putchar (i);
 	}
 
-	printf("\n");
+	putchar ('\n');
 
 	return (0);
 }
