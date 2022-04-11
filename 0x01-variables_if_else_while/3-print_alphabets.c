a1c02ec311e2a57e40392f3bcf05784b6274f05d
emawrites <talkema2@gmail.com>
2022-04-11 07:02:18 -0700

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,17 @@
  * Return: Always 0 (Successful)
  */
 
-int main (void)
+int main(void)
 {
 	char lower_case;
 	char upper_case;
 
-	for(upper_case = 'A'; upper_case <= 'Z' : upper_case++)
+	for (upper_case = 'A'; upper_case <= 'Z' ; upper_case++)
 	{
 		putchar(upper_case);
 	}
 
-	for(lower_case = 'a'; lower_case <= 'z' : lower_case++)
+	for (lower_case = 'a'; lower_case <= 'z' ; lower_case++)
 	{
 		putchar(lower_case);
 	}
