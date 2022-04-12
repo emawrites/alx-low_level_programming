8c95fb67fdb8d2226d6c5ea4ec5b71088c20a2fa
emawrites <talkema2@gmail.com>
2022-04-12 08:16:26 -0700

    taks 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - functions that adds two integers together.
+ * @a: an integer
+ * @b: an integer
+ * Return: result of a + b.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
