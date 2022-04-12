e2655b2e3a4976a5468c12696070c3726d092950
emawrites <talkema2@gmail.com>
2022-04-12 04:17:42 -0700

    task 12 new

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int ema1, ema2;
+
+	for (ema1 = 0; ema1 <= 98; ema1++)
+	{
+	for (ema2 = num1 + 1; ema2 <= 99; ema2++)
+	{
+		putchar((ema1 / 10) + '0');
+		putchar((ema1 % 10) + '0');
+		putchar(' ');
+		putchar((ema2 / 10) + '0');
+		putchar((ema2 % 10) + '0');
+
+	if (ema1 == 98 && ema2 == 99)
+		continue;
+
+	putchar(',');
+	putchar(' ');
+	}
+	}
+
+	putchar('\n')
+
+	return (0);
+}
