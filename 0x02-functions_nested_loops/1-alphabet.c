500eccbb833e11bc4b749f6be6d62309c07c7ab6
emawrites <talkema2@gmail.com>
2022-04-12 06:34:59 -0700

    correcion two

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
-/** 
- * main - Entry point
- * Task: Write a function that prints the alphabet, in lowercase, followed by a new line.
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  * Return: Always 0.
  */
 
