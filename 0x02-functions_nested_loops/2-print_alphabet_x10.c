6cafcf92e9522b30c4a2b626e62aa3b3de87e060
emawrites <talkema2@gmail.com>
2022-04-12 06:44:33 -0700

    new corr

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 -  a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+ * print_alphabet_x10 -  a function that prints 10 times the alphabet, in lowercase.
  */
 
 void print_alphabet_x10(void)
@@ -11,8 +11,8 @@ void print_alphabet_x10(void)
 	
 	while (count++ <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-			_putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+	_putchar('\n');
 	}
 }
