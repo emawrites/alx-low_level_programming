bf1d6a4916d69231126d3315442a88983728bdd6
emawrites <talkema2@gmail.com>
2022-04-12 07:54:33 -0700

    tasl 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: takes ln an integer
+ * Return: output
+ */
+
+int print_last_digit(int n)
+{
+	int ln = n % 10;
+
+	if (n < 0)
+		ln = ln * -1;
+	_putchar(ln _ '0');
+	return (ln);
+}
