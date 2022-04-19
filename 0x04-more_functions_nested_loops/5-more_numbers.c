2b7e04f2e59bdf4ee561cfa9442313c127a2b090
emawrites <talkema2@gmail.com>
2022-04-19 08:06:46 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints nos from 0 -14, 10 times
+ *
+ * Return: void.
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+	for (i = 0; i <= 1; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	if (!(i == 1 && j >= 5))
+	{
+		if (i)
+		_putchar(i + '0');
+		_putchar(j + '0');
+	}
+	}
+	}
+	}
+
+	_putchar('\n');
+}
