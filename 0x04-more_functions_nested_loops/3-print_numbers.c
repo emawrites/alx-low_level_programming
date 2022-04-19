b1515fb163c5d5b8004e361626760b8d2a8ac9ce
emawrites <talkema2@gmail.com>
2022-04-19 04:44:50 -0700

    task 4 update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,9 +11,15 @@ void print_numbers(void)
 	int i;
 
 	i = 0;
-	for (i = 0; i < 10; i++)
+while (i < 10);
+
 	{
-	_putchar(i + '0');
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+		i++;
 	}
-	_putchar('\n');
+
+	_putchar ('\n')
 }
