60b8491722fb77cc1147e3cc893cc4a2b5a0313d
emawrites <talkema2@gmail.com>
2022-04-19 03:36:55 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: takes first integer
+ * @b: second integer
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
