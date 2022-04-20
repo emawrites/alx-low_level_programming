984cc5688a634fca4d9496dda7a3f11476ad5986
emawrites <talkema2@gmail.com>
2022-04-20 10:29:30 -0700

    task 8 updated

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,16 +10,18 @@
 
 void print_array(int *a, int *n)
 {
-        int i;
+	int i;
 
-        for (i = 0; i < n; i++)
-        {
-                printf("%d", a[i]);
-                if (i < (n - 1))
-		{
-			printf(", ");
-		}
-		i++;
+	i = 0;
+	while (i < n)
+	{
+	printf("%d", a[i]);
+
+	if (i < (n - 1))
+	{
+	printf(", ");
+	}
+	i++;
 	}
 
 	printf("\n");
