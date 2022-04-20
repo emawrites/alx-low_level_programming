0f2e0243395fe60cc5801fd6d335b1165be5bcfd
emawrites <talkema2@gmail.com>
2022-04-20 08:21:34 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: points to an int
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int t;
+
+	t = 0;
+	while (s[u] != '\n')
+	{
+		t++;
+	}
+	return (u);
+}
