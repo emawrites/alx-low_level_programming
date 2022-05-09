70592713ea840bfb172171fdbc6527b739c9b25f
emawrites <talkema2@gmail.com>
2022-05-09 04:12:04 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: The number of bytes to be allocated
+ * Return: None(Void function)
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+
+	return (mem);
+}
