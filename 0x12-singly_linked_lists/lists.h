f5d54fae67361b8bf79b70860a72dd96d7e9ed1e
emawrites <talkema2@gmail.com>
2022-05-25 15:42:31 -0700

    task update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,22 +1,22 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _LIST_H_
+#define _LIST_H_
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * struct list_s - singly linked list
- * @n: integer
+ * struct list_t - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
- * Description: node structure on singly linked list
- * A task on alx se program.
+ * Description: singly linked list node structure
+ * for ALX project.
  */
-
-typedef struct list_s
+typedef struct list_t
 {
-	int n;
+	char *str;
+	unsigned int len;
 	struct list_s *next;
 } list_t;
 
@@ -25,4 +25,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
